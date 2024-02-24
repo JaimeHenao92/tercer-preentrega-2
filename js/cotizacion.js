e69d14ca0b217051e9eed50db2e22c25d33e1674
JaimeHenao92 <jaimehenao92@gmail.com>
2024-02-24 11:02:19 -0500

    cargafinal

--- js/cotizacion.js
+++ js/cotizacion.js
@@ -1,4 +1,3 @@
-
 // me enlazo con los elementos HTML del detalle del préstamo
 const spanMonto = document.querySelector("span.monto");
 const spanDestino = document.querySelector("span.destino");
@@ -6,28 +5,66 @@ const spanTasa = document.querySelector("span.intereses");
 const spanPlazo = document.querySelector("span.plazo");
 const spanCuota = document.querySelector("span.cuota");
 const spanTotalDevolver = document.querySelector("span.total");
+const imagenContratar = '<img src="../fotos/animacion-cargando.gif">';
 const btnContratar = document.querySelector("button.button-contratar");
 const divMensajeFinal = document.querySelector("div#panelMensaje");
 
 // lógica
 function recuperarDeLS() {
   const dtsDelPrestamo = JSON.parse(localStorage.getItem("DatosDelPrestamo"));
+
   if (dtsDelPrestamo !== "" && dtsDelPrestamo !== null) {
-      spanMonto.textContent = dtsDelPrestamo.monto.toLocaleString("es-CO");
+      const formatter = new Intl.NumberFormat("es-CO", {
+          style: "currency",
+          currency: "COP",
+          minimumFractionDigits: 0,
+          
+      });
+
+      const formatNumberWithPoints = (number) => {
+          return formatter.format(number);
+      };
+
+      spanMonto.textContent = formatNumberWithPoints(dtsDelPrestamo.monto) + " COP";
       spanDestino.textContent = dtsDelPrestamo.destino;
-      spanTasa.textContent = parseFloat(((dtsDelPrestamo.interes - 1)*100).toFixed(2));
+      spanTasa.textContent = parseFloat(((dtsDelPrestamo.interes - 1) * 100).toFixed(2));
       spanPlazo.textContent = parseInt(dtsDelPrestamo.plazo);
-      spanCuota.textContent = parseFloat(dtsDelPrestamo.cuota.toFixed(2));
-      spanTotalDevolver.textContent = parseFloat(((dtsDelPrestamo.cuota * dtsDelPrestamo.plazo).toFixed(2)).toLocaleString("es-Co"));
+      spanCuota.textContent = formatNumberWithPoints(dtsDelPrestamo.cuota) + " COP";
+      spanTotalDevolver.textContent = formatNumberWithPoints(dtsDelPrestamo.cuota * dtsDelPrestamo.plazo) + " COP";
   }
 }
 
+
+
 //Eventos
 btnContratar.addEventListener("click", () => {
-  divMensajeFinal.classList.add("transition-div-show");
-  localStorage.removeItem("dtssDelPrestamo");
+  // Deshabilitar el botón y cambiar su contenido
   btnContratar.setAttribute("disabled", "true");
+  btnContratar.innerHTML = imagenContratar;
+
+  // temporizador para mostrar el aviso después de 2 segundos
+  setTimeout(() => {
+    limpiarDatosPrestamo();
+    mostrarMensajeFinal();
+  }, 2000);
 });
+
+function limpiarDatosPrestamo() {
+  // Limpiar o restablecer los datos almacenados
+  localStorage.removeItem("DatosDelPrestamo");
+}
+
+function mostrarMensajeFinal() {
+  Swal.fire({
+    title: "¡Contrato exitoso!",
+    text: "Gracias por elegir nuestros servicios.",
+    icon: "success",
+    willClose: () => {
+      // Redirigir al formulario del préstamo después de hacer clic en "OK"
+      location.href = "prestamos.html";
+    }
+  });
+}
 
 // Inicializar la pantalla
 recuperarDeLS();
--- js/index.js
+++ js/index.js
@@ -1,16 +1,13 @@
-const arrayBancos = [
-  { id: 1, descripcion: "BANCOLOMBIA", interes: 1.1821 },
-  { id: 2, descripcion: "BBVA", interes: 1.138 },
-  { id: 3, descripcion: "Banco de Occidente", interes: 1.1021 },
-  { id: 4, descripcion: "Banco Agrario", interes: 1.081 },
-  { id: 5, descripcion: "Banco de Bogota ", interes: 1.112 },
-];
+const URL = "../bancos.json";
+const arrayBancos = [];
 
 const inputMonto = document.querySelector("#montoDinero");
 const inputPlazo = document.querySelector("#cuantoTiempo");
 const selectBancos = document.querySelector("#banco");
+const imagenCargar = '<img src="../fotos/animacion-cargando.gif">';
 const btnCalcular = document.querySelector("#btnCalcular");
 const divMensaje = document.querySelector("#mensaje");
+const contenedorDiv = document.querySelector("div.cotizacion");
 
 function cargarBancos() {
   if (arrayBancos.length > 0) {
@@ -21,7 +18,34 @@ function cargarBancos() {
       selectBancos.appendChild(optionElement);
     });
   }
-}
+};
+
+function retornarCardError() {
+  return `<div class="cotizacion">
+              <p>
+                  ⛔⛔⛔
+              </p>
+              <h3>ERROR AL CARGAR</h3>
+              <h4>POR FAVOR RECARGA LA PÁGINA..</h4>
+          </div>`
+};
+
+function obtenerIntereses() {
+  fetch(URL)
+  .then((response)=> {
+      if (response.ok) {
+          return response.json()
+      } else {
+          throw new Error("No se pudo obtener la información solicitada. (" + response.status + ")")
+      }
+  } )
+  .then((data)=> arrayBancos.push(...data) )
+  .then(()=> cargarBancos())
+  .catch((error)=> {
+    contenedorDiv.innerHTML = retornarCardError()
+  })
+  
+};
 
 function obtenerInteresPorDescripcion(descripcion) {
   const destino = arrayBancos.find(
@@ -65,16 +89,32 @@ function calcularPrestamo() {
 
 // Evento
 btnCalcular.addEventListener("click", () => {
-  let caso1=inputMonto.value;
-  let caso2=inputPlazo.value;
-  let caso3=selectBancos.value
-  if (caso1 !== null && caso2 !== null && caso3 !== "Elige una opción, por favor") {
-    calcularPrestamo()
-  }
-  else {
-    divMensaje.classList.add("transition-div-show");
+  let caso1 = inputMonto.value;
+  let caso2 = inputPlazo.value;
+  let caso3 = selectBancos.value;
+
+  if (caso1 !== "" && caso2 !== "" && caso3 !== "Elige una opción, por favor") {
+      
+      btnCalcular.innerHTML = imagenCargar;
+
+    
+      setTimeout(() => {
+          calcularPrestamo();
+      }, 3000);
+  } else {
+      // Mostrar mensaje de SweetAlert para revisar los datos
+      revisarDatos();
   }
 });
 
+// Función para mostrar el mensaje de SweetAlert
+function revisarDatos() {
+  Swal.fire({
+      title: "Revisar datos",
+      text: "Por favor, completa todos los campos antes de calcular.",
+      icon: "warning"
+  });
+}
+
 // Inicializamos la aplicación
-cargarBancos();
+obtenerIntereses()
